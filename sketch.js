ed98e5bf3fdb053fc9b4bfce6ad0a212da52c408
Anand181-1 <73180243+Anand181-1@users.noreply.github.com>
2020-11-24 21:38:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,6 @@ const Body = Matter.Body;
 var ground;
 var paperBall;
 var part1, part2, part3;
-function preload()
-{
-	
-}
 
 function setup() {
 	createCanvas(800, 700);
@@ -20,12 +16,12 @@ function setup() {
 	world = engine.world;
 
 	ground = new Ground(400, 670, 1000, 10);
-	paperBall = new Paper(50,50,20);
+	paperBall = new Paper(100,600,20);
 	part1 = new Base(600, 657.5);
 	part2 = new Boundries(523,622);
 	part3 = new Boundries(678,622);
-	// var render = Render.create({ element: document.body, engine: engine, options: { width: 1600, height: 700, wireframes: false } });
-	// Render.run(render);
+//	var render = Render.create({ element: document.body, engine: engine, options: { width: 1600, height: 700, wireframes: false } });
+//	Render.run(render);
 	Engine.run(engine);
   
 }
@@ -34,15 +30,12 @@ function setup() {
 function draw() {
   background(0);
   Engine.update(engine);
-  
+
   ground.display();
   paperBall.display(); 
   part1.display();
   part2.display();
-  part3.display();
-
-  drawSprites();
- 
+  part3.display(); 
 }
 
 function keyPressed() {
